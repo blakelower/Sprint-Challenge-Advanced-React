e311ca1f1bb51e76cccebee8a77701ab3c1076f7
blakelower <blakelower1999@gmail.com>
2020-02-14 16:44:45 +0700

    succesfully used custom hooks, I decided to create a dark mode toggle

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Playlist from "./component/Playlist";
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <h1>Womens soccer!</h1>
+      <h2>Scroll to the bottom to toggle dark mode</h2>
       <Playlist />
+      <Navbar/>
     </div>
   );
 }
---
+++ client/src/Hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "../Hooks/useLocalStorage";
+
+export function useDarkMode(initialValue) {
+const [darkMode, setDarkMode] = useLocalStorage(initialValue);
+useEffect(() => {
+darkMode
+? document.body.classList.add("dark-mode")
+: document.body.classList.remove("dark-mode");
+}, [darkMode]);
+console.log("darkmode enabled");
+return [darkMode, setDarkMode];
+}
+
